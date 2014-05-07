52ed136e0f2c62abc5f1fceb8c71c38578d1a251
Chris Martin <chris.martin@eventday.com>
2014-05-07 15:52:26 -0700

    initial commit

---
+++ src/BrokeredMessageAutoLockRenew/BrokeredMessageExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+
+namespace BrokeredMessageAutoLockRenew
+{
+    public static class BrokeredMessageExtensions
+    {
+        public static BrokeredMessageLockRenewer CreateAutoLockRenewer(this BrokeredMessage message)
+        {
+            var tokenSource = new CancellationTokenSource();
+            return new BrokeredMessageLockRenewer(message, tokenSource);
+        }
+
+        public static BrokeredMessageLockRenewer CreateAutoLockRenewer(this BrokeredMessage message, params CancellationToken[] cancellationTokens)
+        {
+            var tokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationTokens);
+            return new BrokeredMessageLockRenewer(message, tokenSource);
+        }
+    }
+}
---
+++ src/BrokeredMessageAutoLockRenew/BrokeredMessageLockRenewer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace BrokeredMessageAutoLockRenew
+{
+    public class BrokeredMessageLockRenewer : IDisposable
+    {
+        private readonly BrokeredMessage _message;
+        private readonly CancellationTokenSource _tokenSource;
+
+        internal BrokeredMessageLockRenewer(BrokeredMessage message, CancellationTokenSource tokenSource)
+        {
+            _message = message;
+            _tokenSource = tokenSource;
+            InitializeRenewal();
+        }
+
+        void IDisposable.Dispose()
+        {
+            Cancel();
+        }
+
+        private void InitializeRenewal()
+        {
+            TimeSpan span = (_message.LockedUntilUtc - DateTime.UtcNow);
+            TimeSpan delay = TimeSpan.FromTicks((long) (span.Ticks*.9));
+
+            Task.Delay(delay, _tokenSource.Token).ContinueWith(t =>
+            {
+                if (_tokenSource.IsCancellationRequested) return;
+
+                _message.RenewLock();
+                InitializeRenewal();
+            }).ContinueWith(t =>
+            {
+                var exception = t.Exception;
+
+                if (exception != null)
+                {
+                    exception.Flatten().Handle(e =>
+                    {
+                        Trace.TraceError(e.Message);
+                        return true;
+                    });
+                }
+            }, TaskContinuationOptions.OnlyOnFaulted)
+                .ConfigureAwait(false);
+        }
+
+        public void Cancel()
+        {
+            _tokenSource.Cancel(false);
+        }
+    }
+}
